fed824dec3f6b9435203d46328bece9b1a48804d
Leoluwa <leoluamid@gmail.com>
2023-06-06 17:29:49 +0100

    f-string ~ String Slicing :)

---
+++ experiments/0x00/task_7/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3, middle_word, word_last_2 = word[:3], word[-8:-1], word[7:]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 Letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
