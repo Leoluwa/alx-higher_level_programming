79623fa24b7767dbe60b016ddf9b9bcc6d9f9eb5
Leoluwa <leoluamid@gmail.com>
2023-06-06 19:25:10 +0100

    String Slicing :) ~ Creating a new string

--- experiments/0x00/task_8/8-concat_edges.py
+++ experiments/0x00/task_8/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
 print(str)
 
 #n = len(str)
@@ -10,4 +9,4 @@ for e in str:
     c += 1
 
 print()
-print(str[39:66], str[114:118], str[0:6])
+print(str[39:67]+str[107:112]+str[0:6])
