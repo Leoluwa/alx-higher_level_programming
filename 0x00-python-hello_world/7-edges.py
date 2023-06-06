39cba5b226265248d1f77f0f52bea503f107c920
Leoluwa <leoluamid@gmail.com>
2023-06-06 17:47:35 +0100

    f-string ~ String Slicing :)

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3, middle_word, word_last_2 = word[:3], word[-8:-1], word[7:]
+word_first_3 = word[:3]
+n, last_2 = len(word), (len(word) - 2)
+middle_word, word_last_2 = word[1:(n-1)], word[last_2:]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 Letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
