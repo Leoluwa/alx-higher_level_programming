cb5f9adec584d086f57c05e476bb8a687f53ddb3
Leoluwa <leoluamid@gmail.com>
2023-06-11 12:40:37 +0100

    Function that prints a string in uppercase followed by a new w!line :)

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    length = len(str)
+    for i in range(0, length):
+        char = ord(str[i])
+        if (char > 96) and (char < 123):
+            print(chr(char - 32), end='')
+        else:
+            print(chr(char), end='')
+    print()
