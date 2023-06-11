cc617109b7c47d75b11ebcc5ab9a98f4fcabdc29
Leoluwa <leoluamid@gmail.com>
2023-06-11 11:19:18 +0100

    Prints the smallest combination of two digits separated by comma, followed by space ~ using the string format :)

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-    for l in range((i + 1), 10):
+    for i_1 in range((i + 1), 10):
         if i == 8:
-            print("{:d}{:d}".format(i, l))
+            print("{:d}{:d}".format(i, i_1))
             break
-        print("{:d}{:d}".format(i, l), end=", ")
+        print("{:d}{:d}".format(i, i_1), end=", ")
