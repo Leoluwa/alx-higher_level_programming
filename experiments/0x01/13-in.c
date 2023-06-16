dd6790b7c520931113ebf15abcf15f20e18da916
Leoluwa <leoluamid@gmail.com>
2023-06-16 23:11:11 +0100

    linked lists :)

---
+++ experiments/0x01/13-in.c
@@ -0,0 +1,13 @@
+/*
+ * 0 - insert in middle
+ *
+ * 1 - empty list
+ *
+ * 2 - insert node at end
+ *
+ * 3 - insert multiple nodes
+ *
+ * 4 - insert duplicate value node
+ *
+ * 5 - insert multiple
+ */
